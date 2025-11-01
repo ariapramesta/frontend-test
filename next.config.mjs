17f057dee36df905741387c69fe21e8a0fd42d9f
Aria Adi Pramesta <ariaapramesta@gmail.com>
2025-11-01 09:55:14 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    turbo: {
+      rules: {},
+    },
+  },
+};
 
 export default nextConfig;
